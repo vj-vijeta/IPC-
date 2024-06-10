b39584423feb34967fb53b2d2a1c7a6f5a3405ad
Vijeta <74856615+vj-vijeta@users.noreply.github.com>
2024-06-10 12:03:18 +0530

    Update server.py

--- ipc-unix-sockets/server.py
+++ ipc-unix-sockets/server.py
@@ -4,37 +4,41 @@ import os
 SERVER_ADDRESS = '/tmp/ipc_socket'
 
 def start_server():
-    # Make sure the socket does not already exist
+    # Ensure the socket does not already exist
     try:
         os.unlink(SERVER_ADDRESS)
+    except FileNotFoundError:
+        pass
     except OSError:
         if os.path.exists(SERVER_ADDRESS):
             raise
 
-    # Create a UDS (Unix Domain Socket)
+    # Create a Unix Domain Socket
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-        # Bind the socket to the address
-        sock.bind(SERVER_ADDRESS)
+        try:
+            # Bind the socket to the address
+            sock.bind(SERVER_ADDRESS)
+            # Listen for incoming connections
+            sock.listen(1)
+            print(f'Server listening on {SERVER_ADDRESS}')
 
-        # Listen for incoming connections
-        sock.listen(1)
-        print(f'Server listening on {SERVER_ADDRESS}')
-
-        while True:
-            # Wait for a connection
-            connection, client_address = sock.accept()
-            try:
-                print('Client connected:', client_address)
-                
-                while True:
-                    data = connection.recv(1024)
-                    if data:
-                        print('Received:', data.decode())
-                        connection.sendall(data)  # Echo the data back to the client
-                    else:
-                        break
-            finally:
-                connection.close()
+            while True:
+                # Wait for a connection
+                connection, client_address = sock.accept()
+                with connection:
+                    print('Client connected')
+                    while True:
+                        data = connection.recv(1024)
+                        if data:
+                            print('Received:', data.decode())
+                            connection.sendall(data)  # Echo the data back to the client
+                        else:
+                            break
+        except Exception as e:
+            print(f'Error: {e}')
+        finally:
+            if os.path.exists(SERVER_ADDRESS):
+                os.unlink(SERVER_ADDRESS)
 
 if __name__ == '__main__':
     start_server()
